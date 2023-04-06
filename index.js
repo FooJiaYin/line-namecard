9d9d4998824c5d29e6c3972448839ad617fd0039
Foo Jia Yin <candy23198@hotmail.com>
2023-04-07 00:24:48 +0800

    feat: Send sample message

--- index.js
+++ index.js
@@ -7,6 +7,25 @@ document.addEventListener("DOMContentLoaded", function() {
     .init({ liffId: process.env.LIFF_ID })
     .then(() => {
         console.log("Success! you can do something with LIFF API here.")
+        /* Login to LIFF */
+		if (!liff.isLoggedIn()) {
+		    console.log("Please login");
+		    liff.login({ redirectUri: process.env.LIFF_URL });
+		} else {
+		    if ( liff.isApiAvailable('shareTargetPicker')) {
+		        liff.shareTargetPicker(sample_message)
+				.then(function (res) {
+					if (res) {
+					  console.log(`[${res.status}] Message sent!`);
+					} else {
+					  console.log("TargetPicker was closed!");
+					}
+				  })
+				  .catch(function (error) {
+					console.log("something wrong happen");
+				  });
+		    }
+		}
     })
     .catch((error) => {
         console.log(error)
