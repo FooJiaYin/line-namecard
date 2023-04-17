cc856ddb2ea26d9eb0c40f97b6e411fdce6c24d1
Foo Jia Yin <candy23198@hotmail.com>
2023-04-17 21:09:36 +0800

    feat: Add message preview

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/flex2html.css"
 import { LiffProvider } from '../context/LiffContext';
 // import { initLiff } from "../api/line/liff";
 import { useState, useEffect } from "react";
