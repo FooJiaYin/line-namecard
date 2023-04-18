12266929de810a552d26315b67e9bfd13dc9ef71
Foo Jia Yin <candy23198@hotmail.com>
2023-04-18 23:17:29 +0800

    feat: Create Page styling

--- pages/create.js
+++ pages/create.js
@@ -26,45 +26,64 @@ export default function Create() {
   };
 
   return (
-    <div className="container">
-      <form>
-        <label>公司名稱</label>
-        <input type="text" value={data.company} onChange={(e) => setData({ ...data, company: e.target.value })} />
-        <label>公司名稱(英文)</label>
-        <input type="text" value={data.companyEN} onChange={(e) => setData({ ...data, companyEN: e.target.value })} />
-        <label>公司Logo</label>
-        <input type="text" value={data.logo} onChange={(e) => setData({ ...data, logo: e.target.value })} />
-        <label>姓名</label>
-        <input type="text" value={data.name} onChange={(e) => setData({ ...data, name: e.target.value })} />
-        <label>姓名(英文)</label>
-        <input type="text" value={data.nameEN} onChange={(e) => setData({ ...data, nameEN: e.target.value })} />
-        <label>職稱</label>
-        <input type="text" value={data.title} onChange={(e) => setData({ ...data, title: e.target.value })} />
-        <label>職稱(英文)</label>
-        <input type="text" value={data.titleEN} onChange={(e) => setData({ ...data, titleEN: e.target.value })} />
-        <label>Email</label>
-        <input type="text" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
-        <label>電話號碼</label>
-        <input type="text" value={data.phone} onChange={(e) => setData({ ...data, phone: e.target.value })} />
-        <label>地址</label>
-        <input type="text" value={data.address} onChange={(e) => setData({ ...data, address: e.target.value })} />
-        <label>地址(英文)</label>
-        <input type="text" value={data.addressEN} onChange={(e) => setData({ ...data, addressEN: e.target.value })} />
-        <label>網頁</label>
-        <input type="text" value={data.website} onChange={(e) => setData({ ...data, website: e.target.value })} />
-        <label>統一編號</label>
-        <input type="text" value={data.taxId} onChange={(e) => setData({ ...data, taxId: e.target.value })} />
+    <div
+      className="row padded"
+      style={{ height: "100vh", alignItems: "center" }}
+    >
+      <div style={{ flex: 2 }}>
+        <form style={{ padding: 20 }}>
+          <div className="row">
+            <div>
+              <label>公司名稱</label>
+              <input type="text" value={data.company} onChange={(e) => setData({ ...data, company: e.target.value })} />
+            </div>
+            <div>
+              <label>公司名稱(英文)</label>
+              <input type="text" value={data.companyEN} onChange={(e) => setData({ ...data, companyEN: e.target.value })} />
+            </div>
+          </div>
+          <label>公司Logo</label>
+          <input type="text" value={data.logo} onChange={(e) => setData({ ...data, logo: e.target.value })} />
+          <div className="row">
+            <div>
+              <label>姓名</label>
+              <input type="text" value={data.name} onChange={(e) => setData({ ...data, name: e.target.value })} />
+            </div>
+            <div>
+              <label>姓名(英文)</label>
+              <input type="text" value={data.nameEN} onChange={(e) => setData({ ...data, nameEN: e.target.value })} />            />
+            </div>
+          </div>
 
-      </form>
-      <MessagePreview message={message} style={{ width: 400 }} />
-      <a
-        onClick={() => send()}
-        target="_blank"
-        rel="noreferrer"
-        className="home__buttons__button button--primary"
-      >
-        Send
-      </a>
+          <div className="row">
+            <div>
+              <label>職稱</label>
+              <input type="text" value={data.title} onChange={(e) => setData({ ...data, title: e.target.value })} />
+            </div>
+            <div>
+              <label>職稱(英文)</label>
+              <input type="text" value={data.titleEN} onChange={(e) => setData({ ...data, titleEN: e.target.value })} />
+            </div>
+          </div>
+          <label>Email</label>
+          <input type="text" value={data.email} onChange={(e) => setData({ ...data, email: e.target.value })} />
+          <label>電話號碼</label>
+          <input type="text" value={data.phone} onChange={(e) => setData({ ...data, phone: e.target.value })} />
+          <label>地址</label>
+          <input type="text" value={data.address} onChange={(e) => setData({ ...data, address: e.target.value })} />
+          <label>網頁</label>
+          <input type="text" value={data.website} onChange={(e) => setData({ ...data, website: e.target.value })} />
+          <label>統一編號</label>
+          <input type="text" value={data.taxId} onChange={(e) => setData({ ...data, taxId: e.target.value })} />
+        </form>
+      </div>
+      <div style={{ flex: 1 }}>
+        <label>預覽</label>
+        <MessagePreview message={message} style={{ width: 400 }} />
+        <center>
+          <button onClick={() => send()}>傳送</button>
+        </center>
+      </div>
     </div>
   );
 }
