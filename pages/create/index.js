89bbf67336cc6f88e9f2dd576947635931098546
Foo Jia Yin <candy23198@hotmail.com>
2023-04-21 03:34:05 +0800

    refactor: Rename template & page

--- pages/create/index.js
+++ pages/create/index.js
@@ -4,8 +4,8 @@ import React from 'react'
 export default function Create() {
   return (
     <div style={{display: "flex", flexDirection: "column"}}>
-      <Link href="/create/horizontal">橫式名片</Link>
-      <Link href="/create/vertical">直式名片</Link>
+      <Link href="/create/namecard-horizontal">橫式名片</Link>
+      <Link href="/create/namecard-vertical">直式名片</Link>
       <Link href="/create/apparel">介紹卡片</Link>
     </div>
   )
