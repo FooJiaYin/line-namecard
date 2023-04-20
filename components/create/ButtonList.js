552558716635e6ccf5b19d2a7ab1a9f9cdeeef28
Foo Jia Yin <candy23198@hotmail.com>
2023-04-20 08:41:59 +0800

    feat: Add/remove button

--- components/create/ButtonList.js
+++ components/create/ButtonList.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { FormProvider, useForm } from "../../hooks/useForm";
 import { Input } from "./Input";
+import { MdOutlineAddCircleOutline, MdOutlineRemoveCircleOutline } from "react-icons/md";
 
 export default function ButtonList() {
   const [formData, setFormData] = useForm();
+  const { buttons } = require("../../assets/data/default.json");
 
   const onButtonChange = (button, index) => {
-    console.log("button Changed", button, index)
     formData.buttons[index] = button;
     setFormData({...formData, buttons: formData.buttons});
   };
@@ -19,6 +20,20 @@ export default function ButtonList() {
             <Input label="按鈕文字" field="text" />
             <Input label="按鈕連結" field="url" />
             <Input label="按鈕顏色" field="backgroundColor" type="color" />
+            <div style={{display: "flex", flexDirection: "column", margin: "auto 0"}} >
+              <MdOutlineAddCircleOutline size={20} style={{margin: "8 0"}} color="#407a9a" 
+                onClick={() => {
+                  formData.buttons.splice(index + 1, 0, buttons[1]);
+                  setFormData({...formData, buttons: formData.buttons});
+                }}
+              />
+              <MdOutlineRemoveCircleOutline size={20} color="#D24B4B" 
+                onClick={() => {
+                  formData.buttons.splice(index, 1);
+                  setFormData({...formData, buttons: formData.buttons});
+                }}
+              />
+            </div>
           </div>
         </FormProvider>
       ))}
